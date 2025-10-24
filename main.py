a17dea3b8cca2a9194d72c854c8b31f273ea9558
ItsBaer <baermanchris@gmail.com>
2025-10-24 09:28:35 -0400

    new stuff

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    while True:
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    running = True
+
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")        
         pygame.display.flip()
+        clock.tick(60)
+    
 
 if __name__ == "__main__":
     main()
