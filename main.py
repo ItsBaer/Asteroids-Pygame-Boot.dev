e5843812be9d1dc12881a1b0c097436b39678bee
ItsBaer <baermanchris@gmail.com>
2025-10-24 11:59:46 -0400

    added a player with the ability to move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,6 +10,12 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -18,9 +25,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")        
+            
+        dt = clock.tick(60) / 1000
+
+        updatable.update(dt)
+        
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)   
+       
         pygame.display.flip()
-        clock.tick(60)
     
 
 if __name__ == "__main__":
